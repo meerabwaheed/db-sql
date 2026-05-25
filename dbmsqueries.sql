7395e9909c4c1ebf74b7cf5118fe48ec030476be
meerabwaheed <meerabdev@gmail.com>
2026-05-25 18:33:02 +0500

    selection of students

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -11,4 +11,5 @@ INSERT INTO students VALUES(1,'meerab',19,72000,3.75);
 INSERT INTO students VALUES(2,'aahil',12,2000);
 INSERT INTO students VALUES(3,'mahad',10,2000);
 INSERT INTO students VALUES(4,'faran',9,52000);
+SELECT * From students;
 
