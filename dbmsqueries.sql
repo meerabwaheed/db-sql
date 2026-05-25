e2738466a1bfaec60c8649fb57cfcd51e6685258
meerabwaheed <meerabdev@gmail.com>
2026-05-25 18:31:52 +0500

    faran jaan salary

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -10,5 +10,5 @@ CREATE TABLE students(
 INSERT INTO students VALUES(1,'meerab',19,72000,3.75);
 INSERT INTO students VALUES(2,'aahil',12,2000);
 INSERT INTO students VALUES(3,'mahad',10,2000);
--- INSERT INTO students VALUES(4,'faran',9,52000);
+INSERT INTO students VALUES(4,'faran',9,52000);
 
