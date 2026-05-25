dd340ef30c6f6294a60a965fed3b7d5bd96b4868
meerabwaheed <meerabdev@gmail.com>
2026-05-25 18:31:07 +0500

    ahil salary done

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -8,4 +8,7 @@ CREATE TABLE students(
    cgpa FLOAT
 );
 INSERT INTO students VALUES(1,'meerab',19,72000,3.75);
+INSERT INTO students VALUES(2,'aahil',12,2000);
+-- INSERT INTO students VALUES(3,'mahad',10,2000);
+-- INSERT INTO students VALUES(4,'faran',9,52000);
 
