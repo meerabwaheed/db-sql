1b1bab30007ce71d23d4f829083218f80159cd3d
meerabwaheed <meerabdev@gmail.com>
2026-05-26 11:44:25 +0500

    hello baalch

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -8,7 +8,7 @@ CREATE TABLE students(
    cgpa FLOAT
 );
 INSERT INTO students VALUES(1,'mirza ',19,72000,3.75);
-INSERT INTO students VALUES(2,'aahil',12,2000);
+INSERT INTO students VALUES(2,'balach',12,2000);
 INSERT INTO students VALUES(3,'mahad',10,2000);
 INSERT INTO students VALUES(4,'faran',9,52000);
 SELECT * From students;
