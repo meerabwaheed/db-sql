8b7c670cb81ed6e0714b99d297c5685759989858
meerabwaheed <meerabdev@gmail.com>
2026-05-26 11:48:18 +0500

    updated students here

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -16,6 +16,6 @@ SELECT * FROM students WHERE age > 18;
 SET SQL_SAFE_UPDATES = 0;
 UPDATE students SET Age = 10 WHERE ID = 1;
 UPDATE students SET Age = 12 WHERE ID = 3;
-UPDATE students SET Age = 11 WHERE ID = 3;
+UPDATE students SET Age = 11 WHERE ID = 2;
 
 
