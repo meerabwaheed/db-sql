efafb7fb8548f76d1194184528c809d69488f331
meerabwaheed <meerabdev@gmail.com>
2026-05-26 11:45:14 +0500

    hammad bhai

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -10,7 +10,7 @@ CREATE TABLE students(
 INSERT INTO students VALUES(1,'mirza ',19,72000,3.75);
 INSERT INTO students VALUES(2,'balach',12,2000);
 INSERT INTO students VALUES(3,'ryyan',10,2000);
-INSERT INTO students VALUES(4,'faran',9,52000);
+INSERT INTO students VALUES(4,'hammad',9,52000);
 SELECT * From students;
 SELECT * FROM students WHERE age > 8;
 SET SQL_SAFE_UPDATES = 0;
