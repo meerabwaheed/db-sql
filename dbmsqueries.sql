a7ec5589d33a7327cc8ffc1d0f345de40865f35f
meerabwaheed <meerabdev@gmail.com>
2026-05-26 11:44:01 +0500

    mirza sahb done

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -7,7 +7,7 @@ CREATE TABLE students(
    salary DOUBLE,
    cgpa FLOAT
 );
-INSERT INTO students VALUES(1,'meerab',19,72000,3.75);
+INSERT INTO students VALUES(1,'mirza ',19,72000,3.75);
 INSERT INTO students VALUES(2,'aahil',12,2000);
 INSERT INTO students VALUES(3,'mahad',10,2000);
 INSERT INTO students VALUES(4,'faran',9,52000);
