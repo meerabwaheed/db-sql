e8cc0566fc2bfbbbc56cc2c48c95e20cb93b38fd
meerabwaheed <meerabdev@gmail.com>
2026-05-26 11:44:58 +0500

    ryyan jan

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -9,7 +9,7 @@ CREATE TABLE students(
 );
 INSERT INTO students VALUES(1,'mirza ',19,72000,3.75);
 INSERT INTO students VALUES(2,'balach',12,2000);
-INSERT INTO students VALUES(3,'mahad',10,2000);
+INSERT INTO students VALUES(3,'ryyan',10,2000);
 INSERT INTO students VALUES(4,'faran',9,52000);
 SELECT * From students;
 SELECT * FROM students WHERE age > 8;
