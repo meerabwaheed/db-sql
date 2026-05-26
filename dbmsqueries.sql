eab4aee1d3921b17e25f024d1cfa7743c59e2475
meerabwaheed <meerabdev@gmail.com>
2026-05-26 11:46:12 +0500

    ryyan age is 16

--- dbmsqueries.sql
+++ dbmsqueries.sql
@@ -9,7 +9,7 @@ CREATE TABLE students(
 );
 INSERT INTO students VALUES(1,'mirza ',22,72000,3.75);
 INSERT INTO students VALUES(2,'balach',18,2000);
-INSERT INTO students VALUES(3,'ryyan',10,2000);
+INSERT INTO students VALUES(3,'ryyan',16,2000);
 INSERT INTO students VALUES(4,'hammad',9,52000);
 SELECT * From students;
 SELECT * FROM students WHERE age > 8;
